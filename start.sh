e8a4cb945b30c39911107cc3660328c62c3b6936
Modesto Figuereo Fernandez <figuereo.fernandez.modesto@gmail.com>
2018-06-09 15:42:57 -0400

    Update README.md file.

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
+readonly infrastructure=(
+  "mysql"
+  "mssql"
+  "mariadb"
+  "postgres"
+  "mongo"
+  "couchdb"
+  "neo4j"
+  "redis"
+  "riak-coordinator"
+  "riak-member"
+  "cassandra"
+  "kong-dashboard"
+  "kong"
+);
+docker-compose -f ./docker-compose/infrastructure.yml start "${infrastructure[@]}"
 docker-compose -f ./docker-compose/services.yml start
-docker-compose -f ./docker-compose/infrastructure.yml start
